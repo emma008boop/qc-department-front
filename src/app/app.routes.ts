92fa8654fdcdcf31b36cd2c5f047fb76bf08ae6d
emma <zuluagamoraemmanuel@gmail.com>
2026-07-05 20:38:59 -0500

    feature: hero page structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./features/landing/hero.component/hero.component').then(m => m.HeroComponent)
+  }
+];
